cebab0cb3acf2cbc119fe93e83fb501b04bf0d25
Vinayak-Gawade <vinayakgawade35@gmail.com>
2025-03-20 23:31:56 +0530

    Creating spring boot application

---
+++ src/test/java/com/dsa/core/DsaCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
